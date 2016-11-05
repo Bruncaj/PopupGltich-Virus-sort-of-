6e26d34e69dd53a81818d4088a1a6047a8fc3f04
Bruncaj <worldsnumberoneperson@gmail.com>
2016-11-04 21:07:08 -0400

    Note:
    You have to copy the location of the files to (maters where you put the file).

--- COPY.cmd
+++ COPY.cmd
@@ -1,6 +1,6 @@
 :1
-start Loop.vbs
-start Chrome.vbs
-start OneNote.vbs
-start COPY.bat
+start C:\(maters where you put the file)\Loop.vbs
+start C:\(maters where you put the file)\Chrome.vbs
+start C:\(maters where you put the file)\OneNote.vbs
+start C:\(maters where you put the file)\COPY.bat
 goto 1
